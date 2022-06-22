a613594610681cd963bfa84d867ec7016cd155ae
dcp <changpengdiao@163.com>
2022-06-22 10:01:23 +0800

    doc init

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,29 @@
-///
-/// Make mysql to struct simple with macro
-/// 
-/// # Example
-/// 
-/// #[derive(ModelHelper)]
-/// struct CustomStrcut{
-///     pub c_id : Option<i64>,
-///     pub c_name : Option<String>,
-/// }
-/// 
-/// // conn is MySQL connection
-/// let v:Vec<CustomStrcut> = conn.query_map("select * from xxx",|row:Row|CustomStrcut::mysql_to_vo(row));
-/// 
+
+//! # mysql_helper
+//! Make mysql to struct simple with macro
+//! 
+//! # Example
+//! 
+//! 
+//! 
+//! Create a strcut which the feilds same with mysql-table feilds
+//! 
+//!     use mysql_helper::ModelHelper;
+//!     #[derive(ModelHelper)]
+//!     struct CustomStrcut{
+//!         pub c_id : Option<i64>,
+//!         pub c_name : Option<String>,
+//!     }
+//!     
+//! Conn is mysql connection,Create connection,see [mysql](https://crates.io/crates/mysql).
+//! ModelHelper will helper you create 'mysql_to_vo' method
+//! 
+//!     let v:Vec<CustomStrcut> = conn.query_map("select * from xxx",|row:Row|CustomStrcut::mysql_to_vo(row));
+//!     
+//! 
+
+
+
 use std::str::FromStr;
 
 use syn::{__private::TokenStream, parse_macro_input, DeriveInput};
